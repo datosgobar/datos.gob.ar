95ce7653137ac158786d8825fc6fa13e45fd112d
Ignacio Heredia <ignacio.nh@gmail.com>
2016-02-04 13:35:39 -0300

    Bootstrap 3

--- ckanext/gobar_theme/plugin.py
+++ ckanext/gobar_theme/plugin.py
@@ -11,3 +11,5 @@ class Gobar_ThemePlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'gobar_theme')
+        toolkit.add_resource('fanstatic', 'bootstrap_style')
+        toolkit.add_resource('fanstatic', 'bootstrap_scripts')
