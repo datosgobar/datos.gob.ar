6948dd66fb32c4e7e93160e05173faf8b41e6e94
Ignacio Heredia <ignacio.nh@gmail.com>
2016-02-09 05:02:45 -0300

    Doble tamaño de grilla para paginas de errores

--- ckanext/gobar_theme/js/home/404.js
+++ ckanext/gobar_theme/js/home/404.js
@@ -1,83 +1,29 @@
+var magicNumbers = {
+    rows: 12,
+    fadeInDelay: 200,
+    fadeSpreadDelay: 10
+};
+var circles = $('.circle-container');
+
 function dottedDigitsConfig() {
-    return [
-        [
-            [false, true, true, true, false],
-            [true, false, false, false, true],
-            [true, false, false, false, true],
-            [true, false, false, false, true],
-            [true, false, false, false, true],
-            [false, true, true, true, false]
-        ], [
-            [false, false, true, false, false],
-            [false, true, true, false, false],
-            [false, false, true, false, false],
-            [false, false, true, false, false],
-            [false, false, true, false, false],
-            [false, true, true, true, false]
-        ], [
-            [false, true, true, true, false],
-            [true, false, false, false, true],
-            [false, false, false, true, false],
-            [false, false, true, false, false],
-            [false, true, false, false, false],
-            [true, true, true, true, true]
-        ], [
-            [true, true, true, true, true],
-            [false, false, false, true, false],
-            [false, false, true, false, false],
-            [false, false, false, true, false],
-            [false, false, false, false, true],
-            [false, true, true, true, false]
-        ], [
-            [false, false, false, true, false],
-            [false, false, true, true, false],
-            [false, true, false, true, false],
-            [true, true, true, true, true],
-            [false, false, false, true, false],
-            [false, false, false, true, false]
-        ], [
-            [true, true, true, true, true],
-            [true, false, false, false, false],
-            [true, true, true, true, false],
-            [false, false, false, false, true],
-            [false, false, false, false, true],
-            [true, true, true, true, false]
-        ], [
-            [false, false, false, true, false],
-            [false, false, true, false, false],
-            [false, true, false, false, false],
-            [true, true, true, true, true],
-            [true, false, false, false, true],
-            [false, true, true, true, false]
-        ], [
-            [false, true, true, true, false],
-            [false, false, false, false, true],
-            [false, false, false, true, false],
-            [false, false, true, false, false],
-            [false, false, true, false, false],
-            [false, false, true, false, false]
-        ], [
-            [false, true, true, true, false],
-            [true, false, false, false, true],
-            [false, true, true, true, false],
-            [true, false, false, false, true],
-            [true, false, false, false, true],
-            [false, true, true, true, false]
-        ], [
-            [false, true, true, true, false],
-            [true, false, false, false, true],
-            [true, true, true, true, true],
-            [false, false, false, true, false],
-            [false, false, true, false, false],
-            [false, true, false, false, false]
-        ]
-    ];
+    return {
+        0: [[false,false,false,true,true,true,true,false,false,false],[false,false,true,true,true,true,true,true,false,false],[false,true,true,false,false,false,false,true,true,false],[true,true,false,false,false,false,false,false,true,true],[true,true,false,false,false,false,false,false,true,true],[true,true,false,false,false,false,false,false,true,true],[true,true,false,false,false,false,false,false,true,true],[true,true,false,false,false,false,false,false,true,true],[true,true,false,false,false,false,false,false,true,true],[false,true,true,false,false,false,false,true,true,false],[false,false,true,true,true,true,true,true,false,false],[false,false,false,true,true,true,true,false,false,false]],
+        1: [[false,false,false,false,false,true,false,false,false,false],[false,false,false,false,true,true,false,false,false,false],[false,false,false,true,true,true,false,false,false,false],[false,false,true,true,true,true,false,false,false,false],[false,false,false,false,true,true,false,false,false,false],[false,false,false,false,true,true,false,false,false,false],[false,false,false,false,true,true,false,false,false,false],[false,false,false,false,true,true,false,false,false,false],[false,false,false,false,true,true,false,false,false,false],[false,false,false,false,true,true,false,false,false,false],[false,false,false,true,true,true,true,false,false,false],[false,false,true,true,true,true,true,true,false,false]],
+        2: [[false,false,true,true,true,true,true,true,false,false],[false,true,true,true,true,true,true,true,true,false],[true,true,false,false,false,false,false,false,true,true],[true,false,false,false,false,false,false,false,true,true],[false,false,false,false,false,false,false,true,true,false],[false,false,false,false,false,false,true,true,false,false],[false,false,false,false,false,true,true,false,false,false],[false,false,false,false,true,true,false,false,false,false],[false,false,false,true,true,false,false,false,false,false],[false,false,true,true,false,false,false,false,false,false],[false,true,true,true,true,true,true,true,true,false],[true,true,true,true,true,true,true,true,true,true]],
+        3: [[true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true],[false,false,false,false,false,false,false,false,true,true],[false,false,false,false,false,false,false,true,true,false],[false,false,false,false,false,false,true,true,false,false],[false,false,false,false,true,true,true,false,false,false],[false,false,false,false,false,true,true,false,false,false],[false,false,false,false,false,false,true,true,false,false],[false,false,false,false,false,false,false,true,true,false],[true,true,false,false,false,false,false,false,true,true],[true,true,true,true,true,true,true,true,true,true],[false,true,true,true,true,true,true,true,true,false]],
+        4: [[false,false,false,false,false,false,false,true,true,false],[false,false,false,false,false,false,true,true,true,false],[false,false,false,false,false,true,true,true,true,false],[false,false,false,false,true,true,true,true,true,false],[false,false,false,true,true,true,false,true,true,false],[false,false,true,true,true,false,false,true,true,false],[false,true,true,true,false,false,false,true,true,false],[true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true],[false,false,false,false,false,false,false,true,true,false],[false,false,false,false,false,false,false,true,true,false],[false,false,false,false,false,false,false,true,true,false]],
+        5: [[true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true],[true,true,false,false,false,false,false,false,false,false],[true,true,false,false,false,false,false,false,false,false],[true,true,true,true,true,true,true,true,false,false],[true,true,true,true,true,true,true,true,true,false],[false,false,false,false,false,false,false,true,true,true],[false,false,false,false,false,false,false,false,true,true],[false,false,false,false,false,false,false,false,true,true],[true,true,false,false,false,false,false,false,true,true],[true,true,true,true,true,true,true,true,true,true],[false,true,true,true,true,true,true,true,true,false]],
+        6: [[false,false,false,false,false,false,true,true,false,false],[false,false,false,false,false,true,true,false,false,false],[false,false,false,false,true,true,false,false,false,false],[false,false,false,true,true,false,false,false,false,false],[false,false,true,true,false,false,false,false,false,false],[false,true,true,true,true,true,true,true,false,false],[true,true,true,true,true,true,true,true,true,false],[true,true,true,false,false,false,false,true,true,true],[true,true,false,false,false,false,false,false,true,true],[true,true,false,false,false,false,false,false,true,true],[true,true,true,true,true,true,true,true,true,true],[false,true,true,true,true,true,true,true,true,false]],
+        7: [[true,true,true,true,true,true,true,true,true,true],[true,true,true,true,true,true,true,true,true,true],[false,false,false,false,false,false,false,false,true,true],[false,false,false,false,false,false,false,true,true,false],[false,false,false,false,false,false,true,true,false,false],[false,false,false,false,false,true,true,false,false,false],[false,false,false,false,true,true,false,false,false,false],[false,false,false,true,true,false,false,false,false,false],[false,false,true,true,false,false,false,false,false,false],[false,true,true,false,false,false,false,false,false,false],[true,true,false,false,false,false,false,false,false,false],[true,true,false,false,false,false,false,false,false,false]],
+        8: [[false,true,true,true,true,true,true,true,true,false],[true,true,true,true,true,true,true,true,true,true],[true,true,false,false,false,false,false,false,true,true],[true,true,false,false,false,false,false,false,true,true],[true,true,false,false,false,false,false,false,true,true],[false,true,true,true,true,true,true,true,true,false],[false,true,true,true,true,true,true,true,true,false],[true,true,false,false,false,false,false,false,true,true],[true,true,false,false,false,false,false,false,true,true],[true,true,false,false,false,false,false,false,true,true],[true,true,true,true,true,true,true,true,true,true],[false,true,true,true,true,true,true,true,true,false]],
+        9: [[false,true,true,true,true,true,true,true,true,false],[true,true,true,true,true,true,true,true,true,true],[true,true,false,false,false,false,false,false,true,true],[true,true,false,false,false,false,false,false,true,true],[false,true,true,true,true,true,true,true,true,true],[false,false,true,true,true,true,true,true,true,true],[false,false,false,false,false,false,false,true,true,false],[false,false,false,false,false,false,true,true,false,false],[false,false,false,false,false,true,true,false,false,false],[false,false,false,false,true,true,false,false,false,false],[false,false,false,true,true,false,false,false,false,false],[false,false,true,true,false,false,false,false,false,false]]
+    };
 }
 
 function matchedDigitsFor(error) {
     var dottedFont = dottedDigitsConfig();
     var digits = [];
-    for (var row = 0; row < 6; row++) {
+    for (var row = 0; row < magicNumbers.rows; row++) {
         for (var i = 0; i < error.length; i++) {
             var digit = parseInt(error[i]);
             digits = digits.concat(dottedFont[digit][row]);
@@ -87,16 +33,16 @@ function matchedDigitsFor(error) {
 }
 
 function setRedraw() {
-    var visibleCircles = $('.circle-background.number-visible');
+    var visibleCircles = circles.find('.circle-background.number-visible');
     var randomCircle = visibleCircles[Math.floor(Math.random() * visibleCircles.length)];
-    $(randomCircle).addClass('different').one('click', function (event) {
+    circles.find(randomCircle).addClass('different').one('click', function (event) {
         var $circle = $(event.currentTarget).parents('.circle');
         $circle.find('.circle-background').removeClass('different');
         fadeOut($circle);
-        setTimeout(function() {
+        setTimeout(function () {
             setRedraw();
             fadeIn($circle);
-        }, 400);
+        }, magicNumbers.fadeInDelay);
     });
 }
 
@@ -124,16 +70,15 @@ function toggleFade($el, fadeCallback) {
         '.circle[data-row="' + (row - 1) + '"][data-col="' + col + '"], ' +
         '.circle[data-row="' + row + '"][data-col="' + (col + 1) + '"],' +
         '.circle[data-row="' + row + '"][data-col="' + (col - 1) + '"]';
-    $(selectors).each(function(i, el) {
-        setTimeout(function() {
+    circles.parent().find(selectors).each(function (i, el) {
+        setTimeout(function () {
             fadeCallback($(el));
-        }, 60)
+        }, magicNumbers.fadeSpreadDelay)
     })
 }
 
 $(function () {
     var error = $('#error-code').data('error').toString();
-    var circles = $('.circle-container');
     var digits = matchedDigitsFor(error);
     for (var j = 0; j < circles.length; j++) {
         $(circles[j]).find('.circle-background').toggleClass('visible number-visible', digits[j]);
