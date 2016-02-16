5217665eeb4371cc075ad589973ba8dc0cf1ce89
Ignacio Heredia <ignacio.nh@gmail.com>
2016-02-16 20:53:26 -0300

    Correcciones de estilos en la home

--- ckanext/gobar_theme/js/home/groups.js
+++ ckanext/gobar_theme/js/home/groups.js
@@ -1,9 +1,23 @@
 $(function () {
     var animationDelay = 50;
-    $.each($('.group-container'), function (i, el) {
-        setTimeout(function () {
-            $(el).removeClass('invisible');
-        }, animationDelay * i);
+
+    var calls = [];
+    $('img[data-replace="svg"]').each(function () {
+        var $img = $(this);
+        var imgURL = $img.attr('src');
+        var imgTitle = $img.attr('title');
+        calls.push($.get(imgURL, function (data) {
+            var $svg = $(data).find('svg').attr('title', imgTitle);
+            $svg.find('style').remove();
+            $img.replaceWith($svg);
+        }));
     });
 
+    $.when.apply($, calls).then(function () {
+        $.each($('.group-container'), function (i, el) {
+            setTimeout(function () {
+                $(el).removeClass('invisible');
+            }, animationDelay * i);
+        });
+    });
 });
