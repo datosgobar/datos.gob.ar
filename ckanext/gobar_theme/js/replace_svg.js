baf2b8becc8bffbad0b4f282146f0c55aec6a2eb
Ignacio Heredia <ignacio.nh@gmail.com>
2016-02-22 11:18:02 -0300

    Tooltips para svg de grupos en los filtros de busqueda

--- ckanext/gobar_theme/js/replace_svg.js
+++ ckanext/gobar_theme/js/replace_svg.js
@@ -2,7 +2,7 @@ $(function () {
     function replaceSVG(el) {
         var $img = $(el);
         var imgURL = $img.attr('src');
-        var imgTitle = $img.attr('title');
+        var imgTitle = $img.data('title');
         var imgClass = $img.attr('class');
         var requestParams = {
             url: imgURL,
@@ -12,8 +12,11 @@ $(function () {
             },
             success: function (data) {
                 try {
-                    var $svg = $(data).find('svg').attr('title', imgTitle).attr('class', imgClass);
+                    var $svg = $(data).find('svg').attr('class', imgClass);
+                    var tooltipText = $('<div></div>').text(imgTitle).addClass('tooltip-text');
+                    var tooltip = $('<div></div>').addClass('svg-tooltip').append(tooltipText);
                     $img.replaceWith($svg);
+                    $svg.after(tooltip);
                     $(document).trigger('svg:replace:success', $svg);
                 }
                 catch (err) {
