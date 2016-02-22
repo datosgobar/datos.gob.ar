6923215b627fd56d2f3b02f7e1e59486f64cc055
Ignacio Heredia <ignacio.nh@gmail.com>
2016-02-22 10:53:01 -0300

    Links a filtros por grupos desde la info de cada dataset

---
+++ ckanext/gobar_theme/js/package/groups.js
@@ -0,0 +1,3 @@
+$(function () {
+    $('img[data-replace="svg"]').replaceSVG();
+});
--- ckanext/gobar_theme/js/replace_svg.js
+++ ckanext/gobar_theme/js/replace_svg.js
@@ -1,9 +1,9 @@
 $(function () {
-    console.log('init');
     function replaceSVG(el) {
         var $img = $(el);
         var imgURL = $img.attr('src');
         var imgTitle = $img.attr('title');
+        var imgClass = $img.attr('class');
         var requestParams = {
             url: imgURL,
             method: 'GET',
@@ -12,7 +12,7 @@ $(function () {
             },
             success: function (data) {
                 try {
-                    var $svg = $(data).find('svg').attr('title', imgTitle);
+                    var $svg = $(data).find('svg').attr('title', imgTitle).attr('class', imgClass);
                     $img.replaceWith($svg);
                     $(document).trigger('svg:replace:success', $svg);
                 }
