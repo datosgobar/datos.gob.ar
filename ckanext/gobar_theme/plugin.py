a0fee7ff33ce6192105b5d11290b5d37d02e99d5
Ignacio Heredia <ignacio.nh@gmail.com>
2016-02-24 14:54:14 -0300

    Españolizacion de algunas urls + inhabilitacion y redireccion de otras

--- ckanext/gobar_theme/plugin.py
+++ ckanext/gobar_theme/plugin.py
@@ -2,7 +2,7 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from ckan.plugins import implements, IRoutes
 import ckanext.gobar_theme.helpers as gobar_helpers
-from routes.mapper import SubMapper
+import ckanext.gobar_theme.routing as gobar_routes
 
 
 class Gobar_ThemePlugin(plugins.SingletonPlugin):
@@ -17,74 +17,8 @@ class Gobar_ThemePlugin(plugins.SingletonPlugin):
         toolkit.add_resource('js', 'gobar_js')
 
     def before_map(self, routing_map):
-
-        home_controller = 'ckanext.gobar_theme.controller:GobArHomeController'
-        routing_map.connect('/', controller=home_controller, action='index')
-
-        # Paginas estaticas
-        with SubMapper(routing_map, controller=home_controller) as m:
-            m.connect('gobar_open_data', '/datos-abiertos', action='open_data')
-            m.connect('gobar_legal_planning', '/marco-legal/planificacion', action='legal_planning')
-            m.connect('gobar_legal_system', '/marco-legal/sistema-nacional', action='legal_system')
-            m.connect('gobar_legal_access', '/marco-legal/regimen-de-acceso', action='legal_access')
-            m.connect('gobar_platform_users', '/plataforma/usuarios', action='platform_users')
-            m.connect('gobar_platform_terms', '/plataforma/terminos_y_condiciones_de_uso', action='platform_terms')
-            m.connect('gobar_platform_license', '/plataforma/licencias', action='platform_license')
-        routing_map.redirect('/about', '/datos-abiertos')
-        routing_map.redirect('/marco-legal', '/marco-legal/planificacion')
-        routing_map.redirect('/plataforma', '/plataforma/usuarios')
-
-        # Dashboard de usuario registrado
-        routing_map.redirect('/dashboard', '/')
-        routing_map.redirect('/dashboard/datasets', '/')
-        routing_map.redirect('/dashboard/groups', '/')
-        routing_map.redirect('/dashboard/organizations', '/')
-        routing_map.redirect('/dashboard/{url:.*?}', '/dashboard')
-
-        # Organizaciones
-        organizations_controller = 'ckanext.gobar_theme.controller:GobArOrganizationController'
-        routing_map.connect('/organization', controller=organizations_controller, action='index')
-        routing_map.redirect('/organization/list', '/organization')
-        routing_map.connect('/organization/new', action='new', controller='organization')
-        routing_map.redirect('/organization/{id}', '/organization/list')
-        routing_map.redirect('/organization/activity/{id}/{offset}', '/organization/list')
-        routing_map.redirect('/organization/about/{id}', '/organization/list')
-        routing_map.redirect('/organization/members/{id}', '/organization/list')
-        routing_map.redirect('/organization/bulk_process/{id}', '/organization/list')
-
-        # Grupos
-        routing_map.redirect('/group', '/')
-        routing_map.redirect('/group/list', '/')
-        routing_map.connect('group_new', '/group/new', action='new', controller='group')
-        routing_map.redirect('/group/{id}', '/group/list')
-        routing_map.redirect('/group/about/{id}', '/group/list')
-        routing_map.redirect('/group/members/{id}', '/group/list')
-        routing_map.redirect('/group/activity/{id}/{offset}', '/group/list')
-
-        # Usuarios
-        user_controller = 'ckanext.gobar_theme.controller:GobArUserController'
-        routing_map.connect('/user/edit', action='edit', controller='user')
-        routing_map.redirect('/user/generate_key/{id}', '/')
-        routing_map.redirect('/user/activity/{id}/{offset}', '/')
-        routing_map.redirect('/user/activity/{id}', '/')
-        routing_map.redirect('/user/follow/{id}', '/')
-        routing_map.redirect('/user/unfollow/{id}', '/')
-        routing_map.redirect('/user/followers/{id:.*}', '/')
-        routing_map.connect('user_edit', '/user/edit/{id:.*}', action='edit', ckan_icon='cog', controller='user')
-        routing_map.redirect('/user/delete/{id}', '/')
-        routing_map.redirect('/user/reset/{id:.*}', '/')
-        routing_map.redirect('/user/register', '/')
-        routing_map.connect('login', '/user/login', action='login', controller='user')
-        routing_map.connect('/user/_logout', action='logout', controller='user')
-        routing_map.connect('/user/logged_in', action='logged_in', controller='user')
-        routing_map.connect('/user/logged_out', action='logged_out', controller='user')
-        routing_map.connect('/user/logged_out_redirect', action='logged_out_page', controller='user')
-        routing_map.redirect('/user/reset', '/')
-        routing_map.redirect('/user/me', '/')
-        routing_map.redirect('/user/set_lang/{lang}', '/')
-        routing_map.connect('user_datasets', '/user/{id:.*}', action='read', ckan_icon='sitemap', controller=user_controller)
-        routing_map.redirect('/user', '/')
-
+        gobar_router = gobar_routes.GobArRouter(routing_map)
+        gobar_router.set_routes()
         return routing_map
 
     def get_helpers(self):
---
+++ ckanext/gobar_theme/routing.py
@@ -0,0 +1,129 @@
+from routes.mapper import SubMapper
+
+
+class GobArRouter:
+
+    def __init__(self, route_map):
+        self.route_map = route_map
+
+        self.home_controller = 'ckanext.gobar_theme.controller:GobArHomeController'
+        self.home_routes = SubMapper(self.route_map, controller=self.home_controller)
+
+    def redirect(self, *routes):
+        for url_from, url_to in routes:
+            self.route_map.redirect(url_from, url_to)
+
+    def set_routes(self):
+        self.connect_home()
+        self.connect_static()
+        self.connect_datasets()
+        self.connect_organizations()
+        self.connect_groups()
+        self.connect_users()
+        self.remove_dashboard()
+        self.remove_tags()
+        self.remove_revision()
+
+    def connect_home(self):
+        self.home_routes.connect('/', action='index')
+
+    def connect_static(self):
+        self.home_routes.connect('gobar_open_data', '/datos-abiertos', action='open_data')
+        self.home_routes.connect('gobar_legal_planning', '/marco-legal/planificacion', action='legal_planning')
+        self.home_routes.connect('gobar_legal_system', '/marco-legal/sistema-nacional', action='legal_system')
+        self.home_routes.connect('gobar_legal_access', '/marco-legal/regimen-de-acceso', action='legal_access')
+        self.home_routes.connect('gobar_platform_users', '/plataforma/usuarios', action='platform_users')
+        self.home_routes.connect('gobar_platform_terms', '/plataforma/terminos-y-condiciones', action='platform_terms')
+        self.home_routes.connect('gobar_platform_license', '/plataforma/licencias', action='platform_license')
+        self.redirect(
+            ('/about', '/datos-abiertos'),
+            ('/marco-legal', '/marco-legal/planificacion'),
+            ('/plataforma', '/plataforma/usuarios')
+        )
+
+    def connect_datasets(self):
+        self.route_map.connect('/dataset/{id}/archivo/{resource_id}', action='resource_read', controller='package')
+        self.redirect(
+            ('/dataset/history/{id:.*?}', '/dataset/{id}'),
+            ('/dataset/activity/{id:.*?}', '/dataset/{id}'),
+            ('/dataset/groups/{id:.*?}', '/dataset/{id}'),
+            ('/dataset/followers/{id:.*?}', '/dataset/{id}'),
+            ('/dataset/{id}/resource/{resource_id}', '/dataset/{id}/archivo/{resource_id}')
+        )
+
+    def remove_dashboard(self):
+        self.redirect(
+            ('/dashboard', '/'),
+            ('/dashboard/datasets', '/'),
+            ('/dashboard/groups', '/'),
+            ('/dashboard/organizations', '/'),
+            ('/dashboard/{url:.*?}', '/dashboard')
+        )
+
+    def connect_organizations(self):
+        self.route_map.connect('/organizaciones', action='index', controller='organization')
+        self.route_map.connect('/organization/new', action='new', controller='organization')
+        self.redirect(
+            ('/organization', '/organizaciones'),
+            ('/organization/list', '/organization'),
+            ('/organization/{id}', '/organization/list'),
+            ('/organization/activity/{id}/{offset}', '/organization/list'),
+            ('/organization/about/{id}', '/organization/list'),
+            ('/organization/admins/{id}', '/organization/list'),
+            ('/organization/members/{id}', '/organization/list'),
+            ('/organization/member_new/{id}', '/organization/list'),
+            ('/organization/member_delete/{id}', '/organization/list'),
+            ('/organization/history/{id}', '/organization/list'),
+            ('/organization/bulk_process/{id}', '/organization/list')
+        )
+
+    def connect_groups(self):
+        self.route_map.connect('group_new', '/group/new', action='new', controller='group')
+        self.redirect(
+            ('/group', '/'),
+            ('/group/list', '/'),
+            ('/group/{id}', '/group/list'),
+            ('/group/about/{id}', '/group/list'),
+            ('/group/members/{id}', '/group/list'),
+            ('/group/member_new/{id}', '/group/list'),
+            ('/group/member_delete/{id}', '/group/list'),
+            ('/group/history/{id}', '/group/list'),
+            ('/group/followers/{id}', '/group/list'),
+            ('/group/follow/{id}', '/group/list'),
+            ('/group/unfollow/{id}', '/group/list'),
+            ('/group/admins/{id}', '/group/list'),
+            ('/group/activity/{id}/{offset}', '/group/list')
+        )
+
+    def connect_users(self):
+        self.route_map.connect('login', '/ingresar', action='login', controller='user')
+        self.redirect(
+            ('/user/login', '/ingresar'),
+            ('/user/generate_key/{id}', '/'),
+            ('/user/activity/{id}/{offset}', '/'),
+            ('/user/activity/{id}', '/'),
+            ('/user/follow/{id}', '/'),
+            ('/user/unfollow/{id}', '/'),
+            ('/user/followers/{id:.*}', '/'),
+            ('/user/delete/{id}', '/'),
+            ('/user/reset/{id:.*}', '/'),
+            ('/user/register', '/'),
+            ('/user/reset', '/'),
+            ('/user/set_lang/{lang}', '/'),
+            ('/user', '/')
+        )
+
+    def remove_tags(self):
+        self.redirect(
+            ('/tag', '/'),
+            ('/tag/{url}', '/')
+        )
+
+    def remove_revision(self):
+        self.redirect(
+            ('/revision', '/'),
+            ('/revision/list', '/'),
+            ('/revision/edit/{id}', '/revision'),
+            ('/revision/diff/{id}', '/revision'),
+            ('/revision/{id}', '/revision')
+        )
