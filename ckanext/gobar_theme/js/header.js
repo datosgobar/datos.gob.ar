49a2c2feec07fa9334481aa97df4efec79db969b
Ignacio Heredia <ignacio.nh@gmail.com>
2016-10-18 09:18:42 -0300

    Primera version de navbar en header para resoluciones chicas

--- ckanext/gobar_theme/js/header.js
+++ ckanext/gobar_theme/js/header.js
@@ -6,4 +6,14 @@ $(function () {
             headerNav.removeClass('active')
         });
     });
+
+    $('#header .icon-reorder').on('click', function(e) {
+        $('#header .xs-navbar').toggleClass('hidden')
+        $('#header .icon-reorder').toggleClass('active')
+        $('#header .logo-header').toggleClass('showing-nav')
+    });
+
+    $('#header .dropdown-navbar-link').on('click', function(e) {
+        $('#header .dropdown-navbar').toggleClass('hidden')
+    })
 })
