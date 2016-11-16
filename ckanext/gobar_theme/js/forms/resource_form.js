590634d2a8eccd33b4d3971aed6ed53b5df32269
Ignacio Heredia <ignacio.nh@gmail.com>
2016-11-16 12:15:00 -0300

    Agrego logica a la seccion de de campos de recurso en form de recurso

--- ckanext/gobar_theme/js/forms/resource_form.js
+++ ckanext/gobar_theme/js/forms/resource_form.js
@@ -7,9 +7,39 @@ $(function () {
         $('.resource-attributes-actions').before(newCol);
         $('#remove-col').removeClass('hidden');
     });
+
     $('#remove-col').on('click', function (e) {
         var cols = $('.resource-attributes-group');
         $(e.currentTarget).toggleClass('hidden', cols.length == 2);
         cols.last().remove()
+    });
+
+    function addAttributesHidden() {
+        var attributesGroups = $('.resource-attributes-group');
+        var attributes = [];
+        for (var i = 0; i < attributesGroups.length; i++) {
+            var attributeGroupEl = $(attributesGroups[i]);
+            var attributeGroup = {
+                title: attributeGroupEl.find('.resource-col-name').val(),
+                description: attributeGroupEl.find('.resource-col-descrition').val(),
+                type: (attributeGroupEl.find('.resource-col-type').val() || '')
+            };
+            if (attributeGroup.title.length > 0 || attributeGroup.type.length > 0 || attributeGroup.description.length > 0) {
+                attributes.push(attributeGroup);
+            }
+        }
+        var hiddenEl = $('<input type="hidden">')
+            .val(JSON.stringify(attributes))
+            .attr('name', 'attributesDescription');
+        $('#resource-attributes-form').append(hiddenEl);
+    }
+
+    $('form#resource-edit').on('submit', function (e) {
+        e.preventDefault();
+        var $form = $(this);
+        addAttributesHidden();
+        var hiddenSave = $('<input type="hidden" name="save">');
+        $form.append(hiddenSave);
+        $form[0].submit();
     });
 });
