510cb6f17fa2bcc2daf6ba1969555c660e393dac
chdigiorno <cdigiorno@devartis.com>
2018-03-02 12:08:21 -0300

    Fix render de página about_developers.

--- ckanext/gobar_theme/controller.py
+++ ckanext/gobar_theme/controller.py
@@ -60,7 +60,7 @@ class GobArHomeController(HomeController):
         return base.render('static/about_legal.html')
 
     def about_developers(self):
-        return base.render('apis/apis.html')
+        return base.render('apis/about_developers.html')
 
     def about_glossary(self):
         return base.render('static/about_glossary.html')
